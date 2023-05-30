8280c247b58bfec9814c79ad11087e93cbdc3f3f
Sashi445 <sashidhar652@gmail.com>
2023-05-30 23:46:29 +0530

    fixed issue with image

--- app/components/landing/Intro.jsx
+++ app/components/landing/Intro.jsx
@@ -8,17 +8,17 @@ export const Intro = () => {
         <div className="h-full w-full flex items-center justify-between p-10 border">
             <div>
                 <Typography {...FontVariant.HeadingBold14} color={...colorPalette.popWhite[500]} >
-                    Don’t know what to watch for this weekend?
+                    {/* Don’t know what to watch for this weekend? */}
                 </Typography>
                 <p>
                     Can't recall the movie your friend suggested recently?
                 </p>
                 <p>
-                    Don’t worry we’ve figured it out for you :)
+                    {/* Don’t worry we’ve figured it out for you : 0 */}
                 </p>
             </div>
             <div>
-                <Image alt="someimage" height={100} width={100} />
+                {/* <Image src={''} alt="someimage" height={100} width={100} /> */}
             </div>
         </div>
     );
--- app/page.js
+++ app/page.js
@@ -1,8 +1,5 @@
 "use client";
 
-import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
 import {
   Feature1,
   Feature2,
