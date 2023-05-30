b6c84cdb705f7cb557fec0645d39fc63c667bb33
Sashi445 <sashidhar652@gmail.com>
2023-05-30 23:53:56 +0530

    issue with Intro component resolved

--- app/components/landing/Intro.jsx
+++ app/components/landing/Intro.jsx
@@ -1,17 +1,18 @@
-import { Button, Typography } from "@cred/neopop-web/lib/components";
+import { Typography } from "@cred/neopop-web/lib/components";
 import { FontVariant, colorPalette } from "@cred/neopop-web/lib/primitives";
-import Image from "next/image";
 
 
 export const Intro = () => {
     return (
         <div className="h-full w-full flex items-center justify-between p-10 border">
             <div>
-                <Typography {...FontVariant.HeadingBold14} color={...colorPalette.popWhite[500]} >
-                    {/* Don’t know what to watch for this weekend? */}
-                </Typography>
+                {/* <Typography {...FontVariant.HeadingBold14} color={...colorPalette.popWhite[500]} > */}
+                {
+                    `Don't know what to watch for this weekend?`
+                }
+                {/* </Typography> */}
                 <p>
-                    Can't recall the movie your friend suggested recently?
+                    Can&apos;t recall the movie your friend suggested recently?
                 </p>
                 <p>
                     {/* Don’t worry we’ve figured it out for you : 0 */}
